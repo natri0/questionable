fe8e5d2f044a8970cedf54ba4d56944470f09bc9
Lina <dev@lina.moe>
2025-01-29 12:24:42 +0200

    Fix saddles pt.2
    apparently, it didn't send the packet when dismounting as well -.- so the player that once was ridden would still see the one who once rode them

--- src/main/java/me/nullium21/questionable/mixin/EntityMixin.java
+++ src/main/java/me/nullium21/questionable/mixin/EntityMixin.java
@@ -9,6 +9,7 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
@@ -22,5 +23,11 @@ public class EntityMixin {
     @Inject(method = "startRiding(Lnet/minecraft/entity/Entity;Z)Z", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;addPassenger(Lnet/minecraft/entity/Entity;)V", shift = At.Shift.AFTER))
     public void sendS2CPacketOnStartRiding(Entity entity, boolean force, CallbackInfoReturnable<Boolean> cir) {
         if (entity instanceof ServerPlayerEntity plr) plr.networkHandler.sendPacket(new EntityPassengersSetS2CPacket(entity));
+    }
+
+    @Inject(method = "dismountVehicle", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;removePassenger(Lnet/minecraft/entity/Entity;)V", shift = At.Shift.AFTER))
+    public void sendS2CPacketOnStopRiding(CallbackInfo ci) {
+        Entity self = (Entity) (Object) this;
+        if (self.getVehicle() instanceof ServerPlayerEntity plr) plr.networkHandler.sendPacket(new EntityPassengersSetS2CPacket(self));
     }
 }
