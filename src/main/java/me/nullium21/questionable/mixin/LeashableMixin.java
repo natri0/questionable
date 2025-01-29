dc14abfd5555713612f147ca0c3ab3c83aa2df61
Lina <dev@lina.moe>
2025-01-29 12:37:02 +0200

    Fix leashing not showing for the leashed
    minecraft devs not implementing base entity networking for player entities again -.-

---
+++ src/main/java/me/nullium21/questionable/mixin/LeashableMixin.java
@@ -0,0 +1,30 @@
+package me.nullium21.questionable.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.Leashable;
+import net.minecraft.network.packet.s2c.play.EntityAttachS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Leashable.class)
+public interface LeashableMixin {
+
+    // todo: will this work when multiple players should see the leash? only tested with attacher & attachee
+
+    @Inject(method = "attachLeash(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/Entity;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;sendToOtherNearbyPlayers(Lnet/minecraft/entity/Entity;Lnet/minecraft/network/packet/Packet;)V", shift = At.Shift.AFTER))
+    private static <E extends Entity & Leashable> void sendPacketToPlayerOnAttachLeash(E entity, Entity leashHolder, boolean sendPacket, CallbackInfo ci) {
+        if (entity instanceof ServerPlayerEntity plr) {
+            plr.networkHandler.sendPacket(new EntityAttachS2CPacket(entity, leashHolder));
+        }
+    }
+
+    @Inject(method = "detachLeash(Lnet/minecraft/entity/Entity;ZZ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerChunkManager;sendToOtherNearbyPlayers(Lnet/minecraft/entity/Entity;Lnet/minecraft/network/packet/Packet;)V", shift = At.Shift.AFTER))
+    private static <E extends Entity & Leashable> void sendPacketToPlayerOnDetachLeash(E entity, boolean sendPacket, boolean dropItem, CallbackInfo ci) {
+        if (entity instanceof ServerPlayerEntity plr) {
+            plr.networkHandler.sendPacket(new EntityAttachS2CPacket(entity, null));
+        }
+    }
+}
